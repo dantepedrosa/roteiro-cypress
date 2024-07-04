f532ffa2e086e9e28e0d42cf0cd69ab9f597b134
Dante Pedrosa <dantepedrosa@gmail.com>
2024-07-03 23:42:06 -0300

    Criado 3 tarefas

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,88 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Desfaz uma tarefa concluída', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+        .should('have.length', 2)
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 0)
+  });
+
+  it('Conclui uma tarefa em "Active"', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2)
+
+    cy.get('.todo-list li .toggle')
+      .last()
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
+  it('Edita o nome de uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+  
+    cy.get('.new-todo')
+      .type('Estudar Cypress{enter}');
+  
+    cy.get('.todo-list li label')
+      .dblclick();
+  
+    cy.get('.todo-list li .edit')
+      .clear()
+      .type('Roteiro de ES{enter}');
+  
+    cy.get('.todo-list li')
+      .should('contain', 'Roteiro de ES');
+  });
 });
